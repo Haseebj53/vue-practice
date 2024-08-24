6b0ddb255e8bf8ff2882c704cc98c4895365568b
Haseebj53 <haseebj53@gmail.com>
2024-08-24 16:40:13 +1000

    fetching jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,3 @@
-<script setup>
-import { ref } from "vue";
-
-const name = ref("John Doe");
-const status = ref("active");
-const tasks = ref(["task one", "task two", "task three"]);
-
-const toggleStatus = () => {
-  if (status.value === "active") {
-    status.value = "pending";
-  } else if (status.value === "pending") {
-    status.value = "inactive";
-  } else {
-    status.value = "active";
-  }
-};
-</script>
-
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status === 'active'">user is active</p>
-  <p v-else-if="status === 'pending'">user is pending</p>
-  <p v-else>user is inactive</p>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
-  <br />
-
-  <button @click="toggleStatus">Change Status</button>
+  <h1>Vue Jobs</h1>
 </template>
