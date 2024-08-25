41d7168da9a204bf534e2269ac76428536f706c7
Haseebj53 <haseebj53@gmail.com>
2024-08-26 08:44:02 +1000

    Router, Axios, Json-server

---
+++ src/components/JobListings.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { reactive, defineProps, onMounted } from "vue";
+import JobListing from "@/components/JobLisitng.vue";
+import { RouterLink } from "vue-router";
+import axios from "axios";
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false,
+  },
+});
+const state = reactive({ jobs: [], isLoading: true });
+onMounted(async () => {
+  try {
+    const response = await axios.get("http://localhost:5000/jobs");
+    state.jobs = response.data;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    state.isLoading = false;
+  }
+});
+</script>
+<template>
+  <section class="bg-green-50 px-4 py-10">
+    <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+      Browse Jobs
+    </h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <JobListing
+        v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
+        :key="job.id"
+        :job="job" />
+    </div>
+  </section>
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <RouterLink
+      to="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</RouterLink
+    >
+  </section>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobLisitngs from "@/components/JobListings.vue";
+</script>
+
+<template>
+  <Hero title="Test Title" subtitle="Test Subtitle" />
+  <HomeCards />
+  <JobLisitngs :limit="3" :showButton="true" />
+</template>
